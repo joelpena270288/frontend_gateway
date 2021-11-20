92418e8f09d63bfdc3e6824c9789fb0210849756
Joel <joelpenagonzalez122@gmail.com>
2021-11-19 21:59:04 -0500

    finish

--- src/app/components/gateway/gateway.component.ts
+++ src/app/components/gateway/gateway.component.ts
@@ -7,6 +7,7 @@ import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { PeripheralDevice } from 'src/app/interfaces/peripheral-device';
 import { NotifierService } from 'angular-notifier';
 import dateFormat, { masks } from "dateformat";
+//Interface to load data from the table of gateway
 export interface TableData {
   serial_number: string;
   position: number;
@@ -15,6 +16,7 @@ export interface TableData {
   Actions: string[];
   id: string;
 }
+//Interface to load data from the table of peripheral devices
 export interface TablePeripheralDevice {
   id?: number;
    
@@ -37,18 +39,20 @@ export interface TablePeripheralDevice {
 
 export class GatewayComponent implements OnInit {
   private readonly notifier: NotifierService;
-  //'serial_number', 'name', 'IPv4'
-  ELEMENT_DATA:TableData[]=[];
-  gateways: Gateway[]=[];
-  displayedColumns: string[] = ['position','serial_number', 'name', 'IPv4','Actions'];
-  dataSource = new MatTableDataSource(this.ELEMENT_DATA);
-  gateway_selected = -1;
-
+  //
+  gateways: Gateway[]=[];//arrangement with gateways
+  ELEMENT_DATA:TableData[]=[];//fix to show table with gateways
+ 
+  displayedColumns: string[] = ['position','serial_number', 'name', 'IPv4','Actions'];//Gateway table columns
+  dataSource = new MatTableDataSource(this.ELEMENT_DATA);//gateways table item
+  
+  gateway_selected = -1;//store the id of a gateway
+  //variables to save the data of a gateway to add it
   serial_number = "";
   name_gateway = "";
   ip_gateway = "";
 
-  // add peripheral
+  // variables to save the data of a peripheral device to add it
 
   uid = 0;
   vendor = "";
@@ -59,7 +63,7 @@ export class GatewayComponent implements OnInit {
   // variable to select the mode to add a peripheral device
   modeAddPeropheral = "";
 
-  //delete peripheral
+  //elements to show the table of peripheral devices in the view of adding a gateway
   selectedPeripheral: PeripheralDevice = {UID:-1,date_created: new Date(),status:"",vendor:""};
 
   PERIPHERAL_DEVICE: TablePeripheralDevice[] = [];
@@ -68,7 +72,7 @@ export class GatewayComponent implements OnInit {
 
   collectionSize = this.PERIPHERAL_DEVICE.length;
   peripheral_device_data: TablePeripheralDevice[] = [];
-
+//variable to store the selected gateway
   gatewaySelected: Gateway = {id: "",IPv4: "",name: "",serial_number: "",peripherals_devices: []};
   
   constructor(private readonly gatewayService: GatewayService, public dialog: MatDialog, config: NgbModalConfig, private modalService: NgbModal, notifierService: NotifierService) {
@@ -77,10 +81,11 @@ export class GatewayComponent implements OnInit {
     config.keyboard = false;
     this.refreshPeripheralDevice();
   }
+  //paging the devices table in the add gateway view
   refreshPeripheralDevice() {
     this.peripheral_device_data = this.PERIPHERAL_DEVICE
-      .map((peripheral, i) => ({id: i + 1, ...peripheral}))
-      .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+    .map((peripheral, i) => ({id: i + 1, ...peripheral}))
+    .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
   }
   
   open(content: any, position: number) {
@@ -88,17 +93,18 @@ export class GatewayComponent implements OnInit {
     this.modalService.open(content);
 
   }
+  //Raise the modal to add a gateway
   newGateway(content: any) {
     
     this.modalService.open(content,{ size: 'lg' });
 
   }
-
+//Raise the modal to add a peripheral device
   newPeripheral(addperipheral: any) {
     this.modalService.open(addperipheral);
     this.modeAddPeropheral = "many";
   }
-
+//Method to add a peripheral device
  async NewPeripheral() {
     let status_check = "online";
    
@@ -123,7 +129,8 @@ export class GatewayComponent implements OnInit {
       this.modeAddPeropheral = "";
       this.cleanPeripheral();
     }
-  }
+ }
+  //clear input values
   cleanPeripheral() {
     this.uid = 0;
   this.vendor = "";
@@ -132,12 +139,13 @@ export class GatewayComponent implements OnInit {
   this.offline = "";
   this.status = "";
   }
+   //clear input values
   cleanGateway() {
     this.serial_number = "";
   this.name_gateway = "";
   this.ip_gateway = "";
   }
-  
+  //Method to add a gateway
   async AddGateway() {
     let array_peripheral_device: PeripheralDevice[] = [];
     for (let index = 0; index < this.PERIPHERAL_DEVICE.length; index++) {
@@ -178,7 +186,7 @@ export class GatewayComponent implements OnInit {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
- 
+ //Method to delete a gateway
   async DeleteGateway() {
 
      await this.gatewayService.deleteGateway(this.gateways[this.gateway_selected - 1].id).then(response => (this.gateways = response.data, this.notifier.notify('success', 'The Gateway was successfully removed')))
@@ -190,7 +198,7 @@ export class GatewayComponent implements OnInit {
     this.cleanPeripheral();
     this.cleanGateway();
   }
-
+  //Load the data of the selected gateway
   async viewGateway(viewgateway:any, id: string) {
 
     this.modalService.open(viewgateway,{ size: 'lg' });
@@ -199,15 +207,17 @@ export class GatewayComponent implements OnInit {
     error.response.data.message
   ));
   }
+  //transform date format
   formatDate(dataselected: Date) {
    return dateFormat(dataselected, "fullDate");
     
   }
-
+//lift the modal to add a single device
   addOnePeripheral(addperipheral:any) {
     this.modalService.open(addperipheral);    
     this.modeAddPeropheral = "one";
   }
+  //Create the peripheral device to delete
  async  selectForDeletePeriperal(deleteperipheral: any, peripheral: PeripheralDevice) {
    this.selectedPeripheral.UID = peripheral.UID;
    this.selectedPeripheral.date_created = peripheral.date_created;
@@ -216,6 +226,7 @@ export class GatewayComponent implements OnInit {
    
    this.modalService.open(deleteperipheral);
  }
+ //Method to delete a device
   async DeletePeriperal() {
     let query_result;
     await this.gatewayService.deletePeripheralDevice(this.gatewaySelected.id, this.selectedPeripheral).then(response => (query_result= response.data, this.notifier.notify('success', 'The Peripheral device was successfully removed')))
--- src/app/services/gateway.service.ts
+++ src/app/services/gateway.service.ts
@@ -9,24 +9,29 @@ import { PeripheralDevice } from '../interfaces/peripheral-device';
 export class GatewayService {
 
   constructor() { }
- async getAllGateway():Promise<any> {  
-    
+  //Load all gateways from the database
+ async getAllGateway():Promise<any> {    
   
    return await axios.get("api/gateway");
  }
+  //Delete a gateway
   async deleteGateway(id: string) {
     return await axios.delete("api/gateway/"+id);
   }
+  //Create a gateway
   async createGateway(gateway: Gateway) {
     return await axios.post("api/gateway/" , gateway);
   }
+  //Get a gateway by id
   async getAllByIdGateway(id: string) {
    
     return await axios.get("api/gateway/"+id);
   }
+  //Insert a gateway
   async addPeriperalDevice(id: string, periperalDevice: PeripheralDevice) {
     return await axios.patch("api/gateway/"+id, periperalDevice) ;
   }
+  //Delete a gateway
   async deletePeripheralDevice(id: string, periperal_device: PeripheralDevice) {
     return await axios.patch("api/gateway/delete_peripheral_device/" + id, periperal_device);
   }
